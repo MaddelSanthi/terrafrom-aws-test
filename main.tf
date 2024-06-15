eee25a5aee30d202d6a2b0df2398df96d1662ed0
mvsanthi123 <mvsanthi.devops@gmail.com>
2024-06-15 21:24:00 +0530

    changed main,tf

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
 # main.tf
 
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-  }
-}
-
 provider "null" {}
 
 resource "null_resource" "hello_world" {
   provisioner "local-exec" {
-    command = "echo 'Hello, World!'"
+    command = "echo '${var.message}'"
   }
 }
+
 
